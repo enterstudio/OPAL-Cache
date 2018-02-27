5a85fbccef2bb40835005b1f872197eee732534b
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-02-27 01:12:47 +0000

    Add todos

--- src/controllers/cacheController.js
+++ src/controllers/cacheController.js
@@ -5,10 +5,19 @@ function CacheController() {
 
 CacheController.prototype.postQuery = function(req, res) {
     res.send("Hello World");
+
+    //TODO: Check if query is present in EAE Jobs collection
+    // If it’s there send back result to interface who sends result to user
+    // If it’s not there, cache sends message NOT HERE (then interface (axel) handles it)
+    // Swagger has job model
+    // Cache does not need to store result
+    // NO / NO, RUNNING / YES (Payload)
+
 };
 
 CacheController.prototype.postResult = function(req, res) {
     res.send("Hello World");
+    //TODO: Forward result to interface
 };
 
 module.exports = CacheController;
