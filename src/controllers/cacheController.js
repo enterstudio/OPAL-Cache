fe7806f0db492d09373b1414f91704e458280215
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-02-27 00:36:51 +0000

    Implement basic controller

--- src/controllers/cacheController.js
+++ src/controllers/cacheController.js
@@ -1,13 +1,14 @@
 function CacheController() {
-
+    this.postQuery = CacheController.prototype.postQuery;
+    this.postResult = CacheController.prototype.postResult;
 }
 
 CacheController.prototype.postQuery = function(req, res) {
-
+    res.send("Hello World");
 };
 
 CacheController.prototype.postResult = function(req, res) {
-
+    res.send("Hello World");
 };
 
 module.exports = CacheController;
