208be854f65cfd5902bb3731c288f0309b6d87fd
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-02-27 00:35:41 +0000

    Move index file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 let express = require('express');
 let os = require('os');
-let app = express();
 
 let config = require('../config/opal.cache.config.js');
 let OpalCache = require('./opalCache.js');
 
-//Remove unwanted express headers
-app.set('x-powered-by', false);
-
 let options = Object.assign({}, config);
 let opalCache = new OpalCache(options);
 
-opalCache.start().then(function(cache_router) {
-    app.use(cache_router);
+opalCache.start().then(function(app) {
+    //Remove unwanted express headers
+    app.set('x-powered-by', false);
     app.listen(config.port, function (error) {
         if (error) {
             console.error(error); // eslint-disable-line no-console
