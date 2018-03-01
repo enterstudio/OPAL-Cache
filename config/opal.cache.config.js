11465bc3dadadd35aa4164aebff2ea1a860f6ed5
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-03-01 22:32:46 +0000

    Remove config

--- config/opal.cache.config.js
+++ config/opal.cache.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
+    mongoUrl: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
     port: 80,
     enableCors: true
 };
