744adbb0199f07de8bfbf009d2299dc2e6b24a58
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-05-24 10:43:01 +0100

    Fixing the unit tests as well

--- test/opalCache.test.js
+++ test/opalCache.test.js
@@ -16,7 +16,7 @@ afterAll(function ()  {
 });
 
 describe('POST /query', () => {
-    test("Cache response is 400 when request does not contain query", async () => {
+    test('Cache response is 400 when request does not contain query', async () => {
         expect.assertions(1);
 
         return request(cacheTestServer.opalCache.app)
@@ -27,16 +27,16 @@ describe('POST /query', () => {
                 expect(res.body.error).toEqual('Request does not contain a job in its body');
             })
     });
-    test("Cache response contains a not null result and waiting set to false when query has been submitted before and is completed", async () => {
+    test('Cache response contains a not null result and waiting set to false when query has been submitted before and is completed', async () => {
         expect.assertions(2);
 
         let query = {
             params: {
-                startDate: '2013-01-01T00:00:00Z',
-                endDate: '2015-12-31T23:59:59Z',
-                algorithm: "density",
-                aggregationLevel: "A",
-                aggregationValues: ["B"],
+                startDate: new Date('2013-01-01T00:00:00Z'),
+                endDate: new Date('2015-12-31T23:59:59Z'),
+                algorithm: 'density',
+                aggregationLevel: 'A',
+                aggregationValues: ['B'],
             },
             status: [Constants.EAE_JOB_STATUS_COMPLETED, Constants.EAE_JOB_STATUS_RUNNING],
             output: [125],
@@ -55,16 +55,16 @@ describe('POST /query', () => {
             })
     });
 
-    test("Cache response contains null result and waiting set to false when query has not been submitted before", async () => {
+    test('Cache response contains null result and waiting set to false when query has not been submitted before', async () => {
         expect.assertions(2);
 
         let submittedQuery = {
             params: {
-                startDate: '2013-01-01T00:00:00Z',
-                endDate: '2015-12-31T23:59:59Z',
-                algorithm: "migration",
-                aggregationLevel: "C",
-                aggregationValues: ["C"],
+                startDate: new Date('2013-01-01T00:00:00Z'),
+                endDate: new Date('2015-12-31T23:59:59Z'),
+                algorithm: 'migration',
+                aggregationLevel: 'C',
+                aggregationValues: ['C'],
             },
             status: [],
             output: [3213],
@@ -74,11 +74,11 @@ describe('POST /query', () => {
 
         let query = {
             params: {
-                startDate: '2013-01-01T00:00:00Z',
-                endDate: '2015-12-31T23:59:59Z',
-                algorithm: "density",
-                aggregationLevel: "A",
-                aggregationValues: ["B"],
+                startDate: new Date('2013-01-01T00:00:00Z'),
+                endDate: new Date('2015-12-31T23:59:59Z'),
+                algorithm: 'density',
+                aggregationLevel: 'A',
+                aggregationValues: ['B'],
             },
             status: [],
             output: [125],
@@ -103,16 +103,16 @@ describe('POST /query', () => {
         [Constants.EAE_JOB_STATUS_SCHEDULED],
         [Constants.EAE_JOB_STATUS_TRANSFERRING_DATA],
         [Constants.EAE_JOB_STATUS_RUNNING],
-    ]).test("Cache response contains null result and waiting set to true when query has been submitted already but job is in status %s", async (status) => {
+    ]).test('Cache response contains null result and waiting set to true when query has been submitted already but job is in status %s', async (status) => {
         expect.assertions(3);
 
         let query = {
             params: {
-                startDate: '2013-01-01T00:00:00Z',
-                endDate: '2015-12-31T23:59:59Z',
-                algorithm: "density",
-                aggregationLevel: "A",
-                aggregationValues: ["C"],
+                startDate: new Date('2013-01-01T00:00:00Z'),
+                endDate: new Date('2015-12-31T23:59:59Z'),
+                algorithm: 'density',
+                aggregationLevel: 'A',
+                aggregationValues: ['C'],
             },
             status: [status],
             output: [125],
